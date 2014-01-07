8619660b87a90761ce6e1b1971ee224c3b74935a
miningold <rynldsrap@gmail.com>
2014-01-07 00:14:40 -0500

    add file printing

--- copy_diff.js
+++ copy_diff.js
@@ -1,9 +1,49 @@
 #!/usr/bin/env node
 
-var mkdirp = require('mkdirp'),
-    stdin = process.stdin;
+var path = require('path'),
+    mkdirp = require('mkdirp'),
+    optimist = require('optimist'),
+    es = require('event-stream'),
+    argv = optimist
+      .usage('Usage: $0')
+      .alias('o', 'output')
+      .describe('o', 'Output path for modified files')
+      .alias('h', 'help')
+      .describe('h', 'Print this usage')
+      .argv,
+    
+    stdin = process.stdin,
+    copy_files,
+    print_files,
+    filter_files;
 
+if (argv.help) {
+  optimist.showHelp();
+  process.exit(0);
+}
+
+var file_list = [];
+stdin.resume();
 stdin.setEncoding('utf8');
-stdin.on('data', function(data) {
-  console.log(data);
-});
+
+var file_pipe = stdin
+  .pipe(es.split())
+  .pipe(es.map(function(file, callback) {
+    var match = file.match(/^[AM]\s(.+)$/);
+
+    if (match) {
+      callback(null, match[1]);
+    } else {
+      callback();
+    }
+  }));
+
+if (argv.output) {
+
+} else {
+  file_pipe
+    .pipe(es.map(function(file, callback) {
+      callback(null, file + '\n');
+    }))
+    .pipe(process.stdout);
+}
