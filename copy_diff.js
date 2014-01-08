2aa31d4d68974e9fb9629a4b8e42e15bbff6ada8
miningold <rynldsrap@gmail.com>
2014-01-07 23:13:43 -0500

    add copy modified functionality

--- copy_diff.js
+++ copy_diff.js
@@ -13,6 +13,7 @@ var path = require('path'),
       .argv,
     
     stdin = process.stdin,
+    global_domain = domain.create(),
     copy_files,
     print_files,
     filter_files;
@@ -22,28 +23,55 @@ if (argv.help) {
   process.exit(0);
 }
 
-var file_list = [];
-stdin.resume();
-stdin.setEncoding('utf8');
+global_domain.run(run);
 
-var file_pipe = stdin
-  .pipe(es.split())
-  .pipe(es.map(function(file, callback) {
-    var match = file.match(/^[AM]\s(.+)$/);
+global_domain.on('error', function(error) {
+  console.error(error);
+  process.exit(1);
+});
 
-    if (match) {
-      callback(null, match[1]);
+var file_pipe;
+
+var copy_file = function(out_dir, in_file) {
+  var abs_out_dir = path.resolve(out_dir, process.cwd());
+  var abs_out_file = path.join(abs_out_dir, in_file);
+
+  mkdirp(abs_out_dir, function(err) {
+    if (err) {
+      file_pipe.emit('error', new Error(err));
     } else {
-      callback();
+      fs.createReadStream(in_file).pipe(fs.createWriteStream(abs_out_file));
     }
-  }));
+  });
+};
 
-if (argv.output) {
+var run = function() {
+  var file_list = [];
+  stdin.resume();
+  stdin.setEncoding('utf8');
 
-} else {
-  file_pipe
+  file_pipe = stdin
+    .pipe(es.split())
     .pipe(es.map(function(file, callback) {
-      callback(null, file + '\n');
-    }))
-    .pipe(process.stdout);
-}
+      var match = file.match(/^[AM]\s(.+)$/);
+
+      if (match) {
+        callback(null, match[1]);
+      } else {
+        callback();
+      }
+    }));
+
+  if (argv.output) {
+    var copy_to_output = copy_file.bind(this, argv.output);
+    file_pipe.on('data', copy_to_output);
+  } else {
+    file_pipe
+      .pipe(es.map(function(file, callback) {
+        callback(null, file + '\n');
+      }))
+      .pipe(process.stdout);
+  }
+};
+
+
