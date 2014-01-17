58e934729c4def2f5fa3a94243dee93c2ed19f42
miningold <rynldsrap@gmail.com>
2014-01-16 19:50:02 -0500

    pull out input and output streams to cli level

--- bin/copy_diff.js
+++ bin/copy_diff.js
@@ -6,4 +6,12 @@ var cli = require('../lib/cli'),
       .alias('h', 'help')
       .argv;
 
+// bind and start stdin
+argv.stdin = process.stdin;
+argv.stdin.resume();
+argv.stdin.setEncoding('utf8');
+
+// bind stdout
+argv.stdout = process.stdout;
+
 cli.argv(argv);
